eac6d2683e5d560f97d31331ffa17c4cb33f8c32
naguirre <naguirre@indiana.edu>
2017-02-23 20:34:26 -0500

    untrack ignored files

---
+++ iu/index.php
@@ -0,0 +1,5 @@
+<?php
+header("Location: http://pages.iu.edu/~naguirre"); /* Redirect browser */
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
---
+++ legacy/index.php
@@ -0,0 +1,5 @@
+<?php
+header("Location: http://pages.iu.edu/~naguirre/legacy"); /* Redirect browser */
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
